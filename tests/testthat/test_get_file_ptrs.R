33bf553c870e5773cfe88271a418fb2e9687c82c
Timm Dankergit config --global user.email timm.danker@nmi.de <you@example.com>
2025-12-20 07:39:14 +0100

    fix tests: test_get_file_ptrs

--- tests/testthat/test_get_file_ptrs.R
+++ tests/testthat/test_get_file_ptrs.R
@@ -1,33 +1,43 @@
 test_that("ptrs work from subdirs", {
-  skip("copy files introduices problems with other tests, skipping until this is fixed")
-  file.copy(ephysdata::examplefile("NaV"), to=".")
-  ephysdata<-read_PATCHMASTER("VG_Blocker.dat")
-  set_file_searchfolder(list("./", "../", "../../"))
   
-  expect_s3_class({
-    suppressWarnings( ephysdata %>% head(1) %>% get_trace() ) 
-  }, "data.frame")
-  
-  
-  dir.create("subdir", showWarnings = F)
-  setwd("./subdir")
-  set_file_searchfolder(list())
-  expect_error(
-    ephysdata %>% head(1) %>% get_trace()  
-  )
-  
-  expect_s3_class({
-    set_file_searchfolder(list("./", "../", "../../"))
-    ephysdata %>% head(1) %>% get_trace()  
-  }, "data.frame")
-  
-  
-  expect_warning({
+
+  testdir <- withr::local_tempdir()
+  withr::with_dir(testdir, {
+    
     file.copy(ephysdata::examplefile("NaV"), to=".")
+    ephysdata<-read_PATCHMASTER("VG_Blocker.dat")
+    
+    # if we include "./" in the searchfolder, we should be able to get the traces: 
     set_file_searchfolder(list("./", "../", "../../"))
-    ephysdata %>% head(1) %>% get_trace()
-    file.remove("VG_Blocker.dat")
-  }, "file was found in more than one place")
+    expect_s3_class({
+      suppressWarnings( ephysdata %>% head(1) %>% get_trace() ) 
+    }, "data.frame")
+    
+    # create and cd into a subdir
+    dir.create("subdir", showWarnings = F)
+    setwd("./subdir")
+    
+    # now, if we unset the searchfolder, we expext an error
+    set_file_searchfolder(list())
+    expect_error(
+      ephysdata %>% head(1) %>% get_trace()  
+    )
+    
+    # but with searchfolder set, it will be found
+    expect_s3_class({
+      set_file_searchfolder(list("./", "../", "../../"))
+      ephysdata %>% head(1) %>% get_trace()  
+    }, "data.frame")
+    
+    # we are still in the subdir, so if we copy our testfile here, it will now be in 2 places,
+    # which should produce a warning:
+    expect_warning({
+      file.copy(ephysdata::examplefile("NaV"), to=".")
+      set_file_searchfolder(list("./", "../", "../../"))
+      ephysdata %>% head(1) %>% get_trace()
+    }, "file was found in more than one place")
+  })
+  
   
   
 })
