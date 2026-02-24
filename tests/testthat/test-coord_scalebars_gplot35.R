748a8ebd0345790396a9dd2f6f1942db2973b435
Danker <Timm.Danker@nmi.de>
2026-02-24 15:11:25 +0100

    removed dependencies from test

--- tests/testthat/test-coord_scalebars_gplot35.R
+++ tests/testthat/test-coord_scalebars_gplot35.R
@@ -1,20 +1,22 @@
 test_that("scalebars auto-length work even when data range is very small", {
-  library(ephys4mini) #fixme this "suggests" dependency has to go away - use generic testdata!
+ 
   # we usually do not have a problem with small data ranges as long as we set xlength and ylength manually. 
   # here, auto-calculation of xlength and ylength is tested for this case
   
   
   # generate example data with very small data range in both x and y axes
-  NaIV_1 <- ephys4Patchmaster::read_PATCHMASTER(ephysdata::examplefile("NaV")) %>% 
-    filter(exp==1, ser==2)  %>%  add_trace(filter_fun = \(y)y/1000)
+  NaIV_1 <- ephys4R:::ephys_testdata() %>% 
+    tidyr::unnest(data) %>% 
+    mutate(y=y/10000) %>% 
+    tidyr::nest(data=c(x,y))
   
   # inner bars:
   vdiffr::expect_doppelganger("inner bars1", {
-    NaIV_1 %>%  ggsweeps() + scalebars(x=.1,y=.1, yunit="pA", xunit = "ms", yfactor=1000, xfactor=1000, expand = F,xlength = .001,ylength = .0001)
+    NaIV_1 %>%  ephys4R::ggsweeps() + scalebars(x=.1,y=.1, yunit="pA", xunit = "ms", yfactor=1000, xfactor=1000, expand = F,xlength = .5,ylength = .00025)
   })
   
   vdiffr::expect_doppelganger("inner bars2", {
-    NaIV_1 %>%  ggsweeps() + scalebars(x=.1,y=.1, yunit="pA", xunit = "ms", yfactor=1000, xfactor=1000, expand = F                               )
+    NaIV_1 %>%  ephys4R::ggsweeps() + scalebars(x=.1,y=.1, yunit="pA", xunit = "ms", yfactor=1000, xfactor=1000, expand = F                               )
     
   })
  
@@ -22,11 +24,11 @@ test_that("scalebars auto-length work even when data range is very small", {
   # outer bars
   
   vdiffr::expect_doppelganger("outer bars1", {
-    NaIV_1 %>%  ggsweeps() + scalebars(yunit="pA", xunit = "ms", yfactor=1000, xfactor=1000, expand = F,xlength = .001,ylength = .0001)
+    NaIV_1 %>%  ephys4R::ggsweeps() + scalebars(yunit="pA", xunit = "ms", yfactor=1000, xfactor=1000, expand = F,xlength = .25,ylength = .0005)
   })
   
   vdiffr::expect_doppelganger("outer bars2", {
-    NaIV_1 %>%  ggsweeps() + scalebars(yunit="pA", xunit = "ms", yfactor=1000, xfactor=1000, expand = F                                       )
+    NaIV_1 %>%  ephys4R::ggsweeps() + scalebars(yunit="pA", xunit = "ms", yfactor=1000, xfactor=1000, expand = F                                       )
   })
   
                            
